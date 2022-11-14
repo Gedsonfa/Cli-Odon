a13413197555be14cf07cf0b79400c813c0ec1ad
Gedsonfa <106558706+Gedsonfa@users.noreply.github.com>
2022-11-13 21:31:44 -0300

    CRUDzim de lei papai
    dor e sofrimento

--- funcoes_agendamentos.c
+++ funcoes_agendamentos.c
@@ -13,7 +13,8 @@ Dados_Agendamento* tela_cadastrar_agendamento(){
     printf("\t===================================================\n");
     printf("\t===============   Cadastrar Agendamento   ============\n");
     printf("\t===================================================\n\n");
-    
+    printf("\t === Insria o codigo do agendamento: ");
+    fgets(age->codigo_agendamento,6,stdin);
     do{
         printf("\t === Insira o CPF do paciente:    ");
         scanf("%15[^\n]", age->cpf_paciente);
@@ -56,24 +57,32 @@ void grava_agendamento(Dados_Agendamento* age){
     fclose(fp);
 }
 
-void tela_pesquisar_agendamento(){
-
+Dados_Agendamento* tela_pesquisar_agendamento() {
+    FILE* fp;
     Dados_Agendamento* age;
-    age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
-
+    char cpf[15];
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t===============   Pesquisar Agendamento   ============\n");
     printf("\t===================================================\n\n");
-    
-    do
-    {
-        printf("\t === Insira o CPF:    ");
-        scanf("%s", age->cpf_paciente);
-        getchar();
-        
-    } while (!validarCPF(age->cpf_paciente));
-    
+    printf("Insira o CPF do paciente: ");
+    scanf("%s", cpf);
+    age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
+    fp = fopen("agendamentos.dat", "rb");
+    if (fp==NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
+    }
+    while (!feof(fp)) {
+        fread(age, sizeof(Dados_Agendamento), 1, fp);
+        if ((age->cpf_paciente == cpf) && (age->status != 'x')) {
+            fclose(fp);
+            return age;
+        }
+    }
+    fclose(fp);
+    return NULL;
     printf("\t==================================================\n\n");
     
     system("\tPause");
@@ -81,24 +90,91 @@ void tela_pesquisar_agendamento(){
 
 }
 
-void tela_alterar_agendamento(){
+void exibeAgendamento (Dados_Agendamento* age) {
+    char situacao[20];
+    if ((age == NULL) || (age->status == 'x')) {
+        printf("\n = Agendamento Inexistente = \n");
+    } else {
+        printf("\n = Agendamento Cadastrado =\n");
+        printf("\nCodigo do agendamento: %s\n", age->codigo_agendamento);
+        printf("\nCPF do paciente: %s\n", age->cpf_paciente);
+        printf("\nCodigo do serviço: %s\n", age->codigo_servico);
+        printf("\nCPF do funcionário: %s\n", age->cpf_funcionario);
+        printf("\nData: %s\n", age->data);
+        printf("\nHora: %s\n", age->hora);
+        if (age->status == 'm') {
+            strcpy(situacao, "Cadastrado");
+        } else if (age->status == 'r') {
+            strcpy(situacao, "Removido");
+        } else {
+            strcpy(situacao, "Não Informado");
+        }
+        printf("\nSituação do agendamento: %s\n", situacao);
+    }
+}
 
+void tela_alterar_agendamento(void) {
+    FILE* fp;
     Dados_Agendamento* age;
-    age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
-
+    int achou;
+    char resp;
+    char procurado[7];
+    fp = fopen("agendamentos.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t===============   Alterar Agendamento   ============\n");
     printf("\t===================================================\n\n");
-    
-    do
-    {
-        printf("\t === Insira o CPF:    ");
-        scanf("%s", age->cpf_paciente);
+    printf("Informe o codigo do agendamento a ser alterado: ");
+    scanf("%6[^\n]", procurado);
+    age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
+    achou = 0;
+    while((!achou) && (fread(age, sizeof(Dados_Agendamento), 1, fp))) {
+        if ((strcmp(age->codigo_agendamento, procurado) == 0) && (age->status == 'm')) {
+            achou = 1;
+        }
+    }
+    if (achou) {
+        exibeAgendamento(age);
         getchar();
-        
-    } while (!validarCPF(age->cpf_paciente));
-    
+        printf("Deseja realmente editar esse agendamento ? (s/n)");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            printf("\t === Insria o codigo do agendamento: ");
+            fgets(age->codigo_agendamento,6,stdin);
+            do {
+                printf("\t === Insira o CPF do paciente:    ");
+                scanf("%15[^\n]", age->cpf_paciente);
+                getchar();
+            } while (!validarCPF(age->cpf_paciente));
+            printf("\t === Insira o codigo do servico:  ");
+            scanf("%6[^\n]", age->codigo_servico);
+            getchar();
+            do {
+                printf("\t === Insira o CPF do funcionario: ");
+                scanf("%15[^\n]", age->cpf_funcionario);
+                getchar();
+            } while (!validarCPF(age->cpf_funcionario));
+            printf("\t === Insira a data:   ");
+            fgets(age->data,11,stdin);
+            printf("\t === Insira o horario:   ");
+            fgets(age->hora,6,stdin);
+            age->status = 'm';
+            fseek(fp, (-1)*sizeof(Dados_Agendamento), SEEK_CUR);
+            fwrite(age, sizeof(Dados_Agendamento), 1, fp);
+            printf("\nAgendamento editado com sucesso!!!\n");
+        } else {
+            printf("\nOK, os dados não foram alterados\n");
+        }
+    } else {
+        printf("O agendamento %s não foi encontrado...\n", procurado);
+      }
+    free(age);
+    fclose(fp);
     printf("\t==================================================\n\n");
     
     system("\tPause");
@@ -106,24 +182,70 @@ void tela_alterar_agendamento(){
 
 }
 
-void tela_excluir_agendamento(){
-
+void listaAgendamento(void) {
+    FILE* fp;
     Dados_Agendamento* age;
+    system ("cls||clear");
+    printf("\t===================================================\n");
+    printf("\t===============   Lista de Agendamentos   ============\n");
+    printf("\t===================================================\n\n");
     age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
+    fp = fopen("agendamentos.dat", "rb");
+    if (fp==NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
+    }
+    while (fread(age, sizeof(Dados_Agendamento), 1, fp)) {
+        if (age->status != 'x') {
+            exibeAgendamento(age);
+        }
+    }
+}
 
+void tela_excluir_agendamento(void) {
+    FILE* fp;
+    Dados_Agendamento* age;
+    int achou;
+    char resp;
+    char procurado[7];
+    fp = fopen("agendamentos.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
+    }
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t===============   Excluir Agendamento   ============\n");
     printf("\t===================================================\n\n");
-    
-    do
-    {
-        printf("\t === Insira o CPF:    ");
-        scanf("%s", age->cpf_paciente);
+    printf("Informe o codigo do agendamento a ser apagado: ");
+    scanf("%6[^\n]", procurado);
+    age = (Dados_Agendamento*) malloc(sizeof(Dados_Agendamento));
+    achou = 0;
+    while ((!achou) && (fread(age, sizeof(Dados_Agendamento), 1, fp))) {
+        if ((strcmp(age->codigo_agendamento, procurado) == 0) && (age->status == 'm')) {
+            achou = 1;
+        }
+    }
+    if (achou) {
+        exibeAgendamento(age);
         getchar();
-        
-    } while (!validarCPF(age->cpf_paciente));
-    
+        printf("Deseja realmente apagar este agendamento ? (s/n)");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            age->status = 'x';
+            fseek(fp, (-1)*sizeof(Dados_Agendamento), SEEK_CUR);
+            fwrite(age, sizeof(Dados_Agendamento), 1, fp);
+            printf("\nAgendamento excluído com sucesso!!!\n");
+        } else {
+            printf("\nOk, os dados não foram alterados\n");
+        }
+    } else {
+        printf("O agendamento %s não foi encontado...", procurado);
+      }
+    free(age);
+    fclose(fp);
     printf("\t==================================================\n\n");
     
     system("\tPause");
--- funcoes_pacientes.c
+++ funcoes_pacientes.c
@@ -4,7 +4,7 @@
 #include "validar.h"
 #include "structs.h"
 
-Dados_Paciente* tela_cadastrar_paciente(){
+Dados_Paciente* tela_cadastrar_paciente(void) {
 
     Dados_Paciente* pac;
     pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
@@ -13,47 +13,28 @@ Dados_Paciente* tela_cadastrar_paciente(){
     printf("\t===================================================\n");
     printf("\t===============   Cadastrar Paciente   ============\n");
     printf("\t===================================================\n\n");
-    /*struct Dados_Agendamento{
-    char cpf_paciente[15];
-    char codigo_servico[6];
-    char cpf_funcionario[15];
-    char data[11];
-    char hora[6];
-    char status;
-};*/
-    do
-    {
-        printf("\t === Insira o CPF So Numeros]:     ");
+    do {
+        printf("\t === Insira o CPF (apenas digitos):     ");
         scanf(" %14[^\n]", pac->cpf);
         getchar();
-        
     } while (!validarCPF(pac->cpf));
-    
-    do{
+    do {
         printf("\t === Insira o nome:   ");
         scanf(" %51[^\n]", pac->nome);
         getchar();
-        
-    }while(!lerLetras(pac->nome));
-
+    } while (!lerLetras(pac->nome));
     printf("\t === Insira a idade:  ");
     scanf(" %20[^\n]", pac->idade);
     getchar();
-    
-
     printf("\t === Insira o endereco:   ");
     scanf( "%51[^\n]",pac->endereco);
     getchar();
-    
-
     printf("\t === Insira o e-mail: ");
     scanf( "%51[^\n]", pac->email);
     getchar();
-
     printf("\t === Insira o numero telefonico:  ");
     scanf("%[0-9]", pac->numero);
     getchar();
-
     pac->status = 'm';
     return pac;
 
@@ -63,7 +44,7 @@ Dados_Paciente* tela_cadastrar_paciente(){
 
 }
 
-void grava_paciente(Dados_Paciente* pac){
+void grava_paciente(Dados_Paciente* pac) {
     FILE* fp;
     fp = fopen("pacientes.dat", "ab");
     if(fp==NULL){
@@ -75,86 +56,193 @@ void grava_paciente(Dados_Paciente* pac){
     fclose(fp);
 }
 
-void tela_pesquisar_paciente(){
-
+Dados_Paciente* tela_pesquisar_paciente(void) {
+    FILE* fp;
     Dados_Paciente* pac;
-    pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
-    
-    char situacao [20];
-    
+    char nome[51];
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t===============   Pesquisar Paciente   ============\n");
     printf("\t===================================================\n\n");
-    
-    if((pac == NULL) || (pac->status == 'x')) {
-        printf("Usuário inexistente");
-    } else {
-    printf(" | Nome: %s\n", pac-> nome);    
-    printf(" | Idade : %s\n", pac-> idade);
-    printf(" | CPF: %s\n", pac-> cpf); 
-    printf(" | E-mail: %s\n", pac-> email); 
-    printf(" | Endereco: %s\n", pac-> endereco); 
-    printf(" | Numero de celular: %s\n", pac-> numero);
-
-
-
-    printf("Situação do usuário: %s\n", situacao);
-
-
-    
-    printf("\t==================================================\n\n");
+    printf("Informe o nome do paciente: ");
+    scanf("%50[^\n]", nome);
+    pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
+    fp = fopen("pacientes.dat", "rb");
+    if(fp==NULL){
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
     }
+    while (!feof(fp)) {
+        fread(pac, sizeof(Dados_Paciente), 1, fp);
+        if ((pac->nome == nome) && (pac->status != 'x')) {
+            fclose(fp);
+            return pac;
+        }
+    }
+    fclose(fp);
+    return NULL;
+    printf("\t==================================================\n\n");
     system("\tPause");
     system("cls | clear");
-
 }
 
-void tela_alterar_paciente(){
+void exibePaciente(Dados_Paciente* pac) {
+    char situacao[20];
+    if ((pac == NULL) || (pac->status == 'x')) {
+        printf("\n = Paciente Inexistente = \n");
+    } else {
+        printf("\n = Paciente Cadastrado = \n");
+        printf("\nCPF: %s\n", pac->cpf);
+        printf("\nNome: %s\n", pac->nome);
+        printf("\nIdade: %s\n", pac->idade);
+        printf("\nEndereço: %s\n", pac->endereco);
+        printf("\nE-mail: %s\n", pac->email);
+        printf("\nNúmero: %s\n", pac->numero);  
+        if (pac->status == 'm') {
+            strcpy(situacao, "Cadastrado");
+        } else if (pac->status == 'r') {
+            strcpy(situacao, "Removido");
+        } else {
+            strcpy(situacao, "Não informado");
+        }
+        printf("\nSituação do paciente: %s\n", situacao);     
+    }
+}
 
+void listaPaciente(void) {
+    FILE* fp;
     Dados_Paciente* pac;
+    printf("\n = Lista de Pacientes =\n");
     pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
+    fp = fopen("pacientes.dat", "rb");
+    if (fp==NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
+    }
+    while (fread(pac, sizeof(Dados_Paciente), 1, fp)) {
+        if (pac->status != 'x') {
+            exibePaciente(pac);
+        }
+    }
+    fclose(fp);
+}
 
+void tela_alterar_paciente(void) {
+    FILE* fp;
+    Dados_Paciente* paciente;
+    int achou;
+    char resp;
+    char procurado[51];
+    fp = fopen("pacientes.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possível continuar o programa...\n");
+        exit(1);
+    }
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t================   Alterar Paciente   =============\n");
-    
     printf("\t===================================================\n\n");
-    do
-    {
-        printf("\t === Insira o CPF:    ");
-        scanf("%s", pac->cpf);
+    printf("Informe o nome do paciente a ser alterado: ");
+    scanf("%50[^\n]", procurado);
+    paciente = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
+    achou = 0;
+    while ((!achou) && fread(paciente, sizeof(Dados_Paciente), 1, fp)) {
+        if ((strcmp(paciente->nome, procurado) == 0) && (paciente->status == 'm')) {
+            achou = 1;
+        }
+    }
+    if (achou) {
+        exibePaciente(paciente);
         getchar();
-        
-    } while (!validarCPF(pac->cpf));
-    
+        printf("Deseja realmente editar esse paciente ? (s/n)");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            do {
+                printf("\t === Insira o CPF (apenas digitos):     ");
+                scanf(" %14[^\n]", paciente->cpf);
+                getchar();
+            } while (!validarCPF(paciente->cpf));
+            do {
+                printf("\t === Insira o nome:   ");
+                scanf(" %51[^\n]", paciente->nome);
+                getchar();
+            } while (!lerLetras(paciente->nome));
+            printf("\t === Insira a idade:  ");
+            scanf(" %20[^\n]", paciente->idade);
+            getchar();
+            printf("\t === Insira o endereco:   ");
+            scanf( "%51[^\n]",paciente->endereco);
+            getchar();
+            printf("\t === Insira o e-mail: ");
+            scanf( "%51[^\n]", paciente->email);
+            getchar();
+            printf("\t === Insira o numero telefonico:  ");
+            scanf("%[0-9]", paciente->numero);
+            getchar();
+            paciente->status = 'm';
+            fseek(fp, (-1)*sizeof(Dados_Paciente), SEEK_CUR);
+            fwrite(paciente, sizeof(Dados_Paciente), 1, fp);
+            printf("\nPaciente editado com sucesso!!!\n");
+        } else {
+                printf("\nOK, os dados não foram alterados\n");
+          }      
+    } else {
+                printf("O paciente %s não foi encontrado...\n", procurado);
+      }
+    free(paciente);
+    fclose(fp);
     printf("\t==================================================\n\n");
-    
     system("\tPause");
     system("cls | clear");
-
 }
 
-void tela_excluir_paciente(){
-
+void tela_excluir_paciente(void) {
+    FILE* fp;
     Dados_Paciente* pac;
-    pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
-
+    int achou;
+    char resp;
+    char procurado[51];
+    fp = fopen("pacientes.dat", "r+b");
+    if (fp == NULL) {
+        printf("Ops! Ocorreu um erro na abertura do arquivo!\n");
+        printf("Não é possivel continuar esse programa...\n");
+        exit(1);
+    }
     system ("cls||clear");
     printf("\t===================================================\n");
     printf("\t==============   Excluir Paciente   ===============\n");
     printf("\t===================================================\n\n");
-   
-    do
-    {
-        printf("\t === Insira o CPF:    ");
-        scanf("%s", pac->cpf);
+    printf("Informe o nome do paciente a ser apagado: ");
+    scanf("%50[^\n]", procurado);
+    pac = (Dados_Paciente*) malloc(sizeof(Dados_Paciente));
+    achou = 0;
+    while ((!achou) && (fread(pac, sizeof(Dados_Paciente), 1, fp))) {
+        if ((strcmp(pac->nome, procurado) == 0) && (pac->status == 'm')) {
+            achou = 1;
+        }
+    }
+    if (achou) {
+        exibePaciente(pac);
         getchar();
-        
-    } while (!validarCPF(pac->cpf));
-    
+        printf("Deseja realmente apagar este paciente ? (s/n)");
+        scanf("%c", &resp);
+        if (resp == 's' || resp == 'S') {
+            pac->status = 'x';
+            fseek(fp, (-1)*sizeof(Dados_Paciente), SEEK_CUR);
+            fwrite(pac, sizeof(Dados_Paciente), 1, fp);
+            printf("\nPaciente excluído com sucesso!!!\n");
+        } else {
+            printf("\nOk, os dados não foram alterados\n");
+          }
+    } else {
+        printf("O paciente %s não foi encontrado...\n", procurado);
+      }
+    free(pac);
+    fclose(fp);
     printf("\t==================================================\n\n");
-    
     system("\tPause");
     system("cls | clear");
 }
